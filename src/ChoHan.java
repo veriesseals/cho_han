a3475dead563f19a7bc8c8f1944ae0f1f398e017
Veries Seals <veries@me.com>
2026-02-22 18:26:35 -0600

    Created player selection menu using print statements

--- src/ChoHan.java
+++ src/ChoHan.java
@@ -1,12 +1,25 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class ChoHan {
     public static void main(String[] args) throws Exception {
         
         // Create a new scanner to read standard input
-        try(Scanner input = new Scanner(System.in)){
-
+        // ---------------------------------------------
+        try(Scanner input = new Scanner(System.in)) {
             
+            // Print welcome message and options
+            // ---------------------------------------------
+            System.out.println("Welcome to Cho Han.");
+
+            System.out.println("1. Even");
+            System.out.println("2. Odd");
+            System.out.println("Select a bet.");
+
+            int playerChoice = input.nextInt();
+            
+
+
+
         }
     }
 }

32cb6ed46a9a839fbba9c5115bb55170117d2b61
Veries Seals <veries@me.com>
2026-02-22 18:15:52 -0600

    Created Try Statement and Scanner object.

--- src/ChoHan.java
+++ src/ChoHan.java
@@ -1,5 +1,12 @@
+import java.util.Scanner
+
 public class ChoHan {
     public static void main(String[] args) throws Exception {
         
+        // Create a new scanner to read standard input
+        try(Scanner input = new Scanner(System.in)){
+
+            
+        }
     }
 }

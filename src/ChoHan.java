e98a671f574aaafa7510fdd62db5ca8367dc7fd1
Veries Seals <veries@me.com>
2026-02-22 18:32:45 -0600

    Created an input object to be read from the player

--- src/ChoHan.java
+++ src/ChoHan.java
@@ -10,11 +10,13 @@ public class ChoHan {
             // Print welcome message and options
             // ---------------------------------------------
             System.out.println("Welcome to Cho Han.");
-
+            
             System.out.println("1. Even");
             System.out.println("2. Odd");
             System.out.println("Select a bet.");
-
+            
+            // Read the players input
+            // ---------------------------------------------
             int playerChoice = input.nextInt();
             
 
